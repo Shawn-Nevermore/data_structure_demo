d6fb7e6e84bd25b34dd41f196246150bfc5861f5
Shawn-Nevermore <shawnxu2016sg@gmail.com>
2017-10-08 16:24:55 +0800

    LinkedQueueOfStrings

---
+++ src/primary/LinkedQueueOfStrings.java
@@ -0,0 +1,56 @@
+package primary;
+
+public class LinkedQueueOfStrings {
+
+	private Node last = null, first = null;
+	
+	private class Node{
+		String item;
+		Node next;
+	}
+	
+	public boolean isEmpty() {
+		return first == null;
+	}
+	
+	public void enqueue(String item) {
+		Node oldLast = last;
+		last = new Node();
+		last.item = item;
+		last.next = null;
+		if(isEmpty()) {
+			first = last;
+		} else {
+			oldLast.next = last;
+		}
+		
+	}
+	
+	public String dequeue() {
+		Node oldFirst = first;
+		first = first.next;
+		String item = oldFirst.item;
+		oldFirst = null;
+		return item;
+	}
+	
+	public static void main(String[] args) {
+		LinkedQueueOfStrings queue = new LinkedQueueOfStrings();
+		queue.enqueue("1");
+		queue.enqueue("2");
+		queue.enqueue("3");
+		queue.enqueue("2");
+		
+		System.out.println(queue.dequeue());
+		System.out.println(queue.dequeue());
+		System.out.println(queue.dequeue());
+				
+		
+		queue.enqueue("12");
+		queue.enqueue("22");
+		queue.enqueue("32");
+		
+		System.out.println(queue.dequeue());
+		System.out.println(queue.dequeue());
+	}
+}
